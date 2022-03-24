d41d2b15e593809976086fb33a16178a4de8543b
ernesto4218 <Ernestosabornido17@gmail.com>
2022-03-24 11:53:35 +0800

    test

--- code2.js
+++ code2.js
@@ -185,7 +185,7 @@ gdjs.LoadDataTestCode.conditionTrue_1.val = runtimeScene.getOnceTriggers().trigg
 }
 
 
-};gdjs.LoadDataTestCode.userFunc0x9e2708 = function(runtimeScene) {
+};gdjs.LoadDataTestCode.userFunc0x9aa360 = function(runtimeScene) {
 "use strict";
 var time_var = runtimeScene.getVariables().get("time_var");
 var date_var = runtimeScene.getVariables().get("date_var");
@@ -270,7 +270,7 @@ gdjs.LoadDataTestCode.eventsList1(runtimeScene);
 {
 
 
-gdjs.LoadDataTestCode.userFunc0x9e2708(runtimeScene);
+gdjs.LoadDataTestCode.userFunc0x9aa360(runtimeScene);
 
 }
 
